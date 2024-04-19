e1232530cf931997b3dbe75bd2315c36b6b260e8
tomek <2002tomo@gmail.com>
2024-04-19 16:36:32 +0200

    Initial Commit

---
+++ PayBackChallenge/PayBackChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayBackChallengeApp.swift
+//  PayBackChallenge
+//
+//  Created by Tomek Lewiński on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PayBackChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
